dd0cd0e4d8b33f8eb95fe1590a4fc35f1ac94d4c
Hao-Nan Zhu <haonan.zhu@ieee.org>
2023-08-07 13:14:09 -0700

    sample csharp console app

---
+++ SampleClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class SampleClass
+    {
+
+        // constructor
+        public SampleClass()
+        {
+            
+            SampleClassName = "Name of SampleClass";
+        }
+
+        public string SampleClassName { get; set; }
+
+        public void SampleMethod()
+        {
+            Console.WriteLine(SampleClassName);
+            SampleClassName = "SampleClass";
+
+        }
+
+    }
+}
